a7e5bcca8ade88e0c52af7c158a221d2b6771de3
doktordan <daniel@pangeaconnect.com>
2014-10-29 19:34:57 +0100

    Menu Modifications

--- public/modules/craiglists/config/craiglists.client.config.js
+++ public/modules/craiglists/config/craiglists.client.config.js
@@ -4,7 +4,8 @@
 angular.module('craiglists').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Craiglists', 'craiglists', 'dropdown', '/craiglists(/create)?');
-		Menus.addSubMenuItem('topbar', 'craiglists', 'Search Craiglists', 'craiglists');
+		//Menus.addMenuItem('topbar', 'Craiglists', 'craiglists', 'dropdown', '/craiglists(/create)?');
+		//Menus.addSubMenuItem('topbar', 'craiglists', 'Search Craiglists', 'craiglists');
+		Menus.addMenuItem('topbar', 'Search', 'craiglists', 'craiglists');
 	}
 ]);
